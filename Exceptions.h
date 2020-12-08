7f0d517c054f06e13fa7b2d23527e4efe19b3181
Mackador <mackadorw@gmail.com>
2020-12-08 09:11:45 -0500

    Created exception header file to store them

---
+++ Exceptions.h
@@ -0,0 +1,31 @@
+//
+// Created by Mackador on 12/8/2020.
+//
+
+#ifndef FINALPROJECT_EXCEPTIONS_H
+#define FINALPROJECT_EXCEPTIONS_H
+
+#include <string>
+#include <sstream>
+
+class IllegalTypeException
+{
+public:
+    IllegalTypeException() {}
+    virtual const char* what() const throw()
+    {
+        return "Illegal Type Exception: Wrong type";
+    }
+};
+
+class NotEnoughCoinsException
+{
+public:
+    NotEnoughCoinsException() {}
+    virtual const char* what() const throw()
+    {
+        return ("Not Enough Coins Exception: You do not have enough coins.");
+    }
+};
+
+#endif //FINALPROJECT_EXCEPTIONS_H
