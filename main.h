6f0f6727ea03221d3165b6cfefa1aa22c27259d3
jahesh davodra <jdavo063@uottawa.ca>
2020-12-07 22:37:38 -0500

    Create new game and add cards

--- main.h
+++ main.h
@@ -2,6 +2,16 @@
 #define MAIN_H
 
 #include <iostream>
+#include <fstream>
+#include "Deck.h"
+#include "Player.h"
+#include "Table.h"
+#include "Chain.h"
+#include "DiscardPile.h"
+#include "TradeArea.h"
+#include "CardFactory.h"
+
+
 using namespace std;
 
 
