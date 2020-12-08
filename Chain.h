21866313d37e4bb363efb6d45a8006d52a87b683
jahesh davodra <jdavo063@uottawa.ca>
2020-12-07 21:25:42 -0500

    Deleted unneeded func

--- Chain.h
+++ Chain.h
@@ -1,88 +1,91 @@
 #ifndef CHAIN_H
 #define CHAIN_H
-#include <iostream>
 #include <vector>
+#include <iostream>
 #include <string>
 #include "Card.h"
 #include "CardFactory.h"
 using namespace std;
 
-
-
 class Chain_Base {
     virtual void set_interface() = 0;
-    char beanType;
+    
 public:
-    char getBeanType() { return beanType; }
+    
 };
 
 template <class T> class Chain {
 public:
-    Chain(istream&, const CardFactory*);
     Chain();
+    Chain(istream&, const CardFactory*);
     Chain<T>& operator+=(Card*);
     int sell();
-    friend ostream& operator<<(ostream& out, const Chain& chain1) {
-        if(chain1.chain.empty()){
-            out << endl;
-            return out;
+    vector<Card*> elems;
+    friend ostream& operator<<(ostream& output, const Chain& printChain) {
+        
+        if(printChain.elems.empty()){
+            output << endl;
+            return output;
         }
-
-        char delimeter = ' ';
-        out << chain1.chain.front()->getName() << delimeter;
-        for (int i = 0; i < chain1.chain.size(); i++) {
-            chain1.chain.front()->print(out);
-            out << delimeter;
+        char spaces = ' ';
+        output << printChain.elems[0]->getName() << spaces;
+        for (int i = 0; i < printChain.elems.size(); i++) {
+            printChain.elems[0]()->print(output);
+            output << spaces;
         }
-        out << endl;
-        return out;
+        output << endl;
+        return output;
     }
     
-    vector<Card*> chain;
+    
 };
 
 template <class T> Chain<T>::Chain() = default;
 
-template <class T> Chain<T>::Chain(istream& istream, const CardFactory* factory) {
-    std::string chainString;
-    getline(istream, chainString);
-
-    string delimiter = " ";
-    // Deletes the bean name
-    int pos = chainString.find(delimiter);
-    chainString.erase(0, pos + delimiter.length());
-    // Remove whitespaces
-    chainString.erase(std::remove(chainString.begin(), chainString.end(), ' '), chainString.end());
-
-    for (int i = 0; chainString[i] != '\0'; i++) {
+template <class T> Chain<T>::Chain(istream& istream, const CardFactory* cFactory) {
+    std::string chLine;
+    getline(istream, chLine);
+    string spaces = " ";
+    int firstSpace = chLine.find(spaces);
+    chLine.erase(0, firstSpace + spaces.length());
+    chLine.erase(std::remove(chLine.begin(), chLine.end(), ' '), chaLine.end());
+    for (int i = 0; chLine[i] != '\0'; i++) {
         Card* card;
-        char val = chainString[i];
-        if (val == 'B')
-        card = new Blue;
-        else if (val == 'C')
-            card = new Chili;
-        else if (val == 'S')
-            card = new Stink;
-        else if (val == 'G')
+        char val = chLine[i];
+        if (val == 'B'){
+            card = new Blue;
+        } 
+        else if (val == 'G'){
             card = new Green;
-        else if (val == 's')
-            card = new Soy;
-        else if (val == 'b')
-            card = new Black;
-        else if (val == 'R')
-            card = new Red;
-        else if (val == 'g')
+        }
+        else if (val == 'C'){
+            card = new Chili;
+        }  
+        else if (val == 'g'){
             card = new Garden;
-        else
+        }
+        else if (val == 's'){
+            card = new Soy;
+        }
+        else if (val == 'S'){
+            card = new Stink;
+        }   
+        else if (val == 'b'){
+            card = new Black;
+        }
+        else if (val == 'R'){
+            card = new Red;
+        }
+        else{
             throw invalid_argument("cardType is invalid");
-
-        chain.push_back(card);
+        }
+        elems.push_back(card);
     }
 }
 
 template <class T> Chain<T>& Chain<T>::operator+=(Card* card) {
     if (card->getName() == T::name){
-        chain.push_back(card);
+        elems.push_back(card);
     } else{
         throw IllegalTypeException();
     }
@@ -94,7 +97,7 @@ template <class T> int Chain<T>::sell() {
     
     for (int coins = 4; coins > 0; i--)
 	{
-		if (chain.size() >= chain[0]->getCardsPerCoin(coins))
+		if (elems.size() >= elems[0]->getCardsPerCoin(coins))
 			return coins;
 	}
 	return 0;
