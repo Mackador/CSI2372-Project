e982612b1618714c23ce6f758fd1bfbdbd023ba7
Mackador <mackadorw@gmail.com>
2020-12-08 20:16:38 -0500

    Updated files

--- Chain.h
+++ Chain.h
@@ -15,14 +15,61 @@ public:
 };
 
 template <class T>
-class Chain : public Chain_Base {
-    vector<T*> cards;
+class Chain : public Chain_Base, public vector<T*> {
 public:
     Chain() = default;
     Chain<T>(istream&, const CardFactory*);
     int sell() const override;
-    Chain_Base& operator+=(Card*) override;
+    Chain<T>& operator+=(Card*) override;
     void print(ostream&) const override;
 };
+
+template <class T>
+Chain<T>::Chain(istream &file, const CardFactory *cardFactory) {
+    string line;
+    file >> line;
+    for (char c : line) {
+        Card* card = cardFactory->stringToCard(c);
+        (*this).push_back((T*)card);
+    }
+}
+
+template <class T>
+Chain<T>& Chain<T>::operator+=(Card* card) {
+    if (card->getName() == T::name) {
+        T* temp = new T();
+        (*this).push_back(temp);
+    } else {
+        throw IllegalTypeException();
+    }
+    return *this;
+}
+
+template <class T>
+int Chain<T>::sell() const {
+    for (int i = 4; i > 0; i--)
+    {
+        if ((*this).size() >= (*this)[0]->getCardsPerCoin(i))
+            return i;
+    }
+    return 0;
+}
+
+template <class T>
+ostream& operator<<(ostream &out, const Chain<T> &chain) {
+    for (T e : chain.cards) {
+        cout << e;
+    }
+    return out;
+}
+
+template <class T>
+void Chain<T>::print(ostream &out) const {
+    out << T::name;
+    out << " ";
+    for (T* t : (*this)) {
+        out << *t;
+    }
+}
 
 #endif //FINALPROJECT_CHAIN_H
